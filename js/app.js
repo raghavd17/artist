9d30e41f90fea9e91b078e0a5bbaced0670a7a8b
raghavd17 <raghavd17@gmail.com>
2015-07-05 15:33:54 +0530

    modification done

--- js/app.js
+++ js/app.js
@@ -25,11 +25,21 @@ var app = angular.module('myApp', []);
 			      });
 			}]);
 
+	function MainNavController($scope, $location) 
+		{ 
+		$scope.isActive = function (viewLocation) { 
+		    return viewLocation === $location.path();
+		};
+	}
+
     // create the controller and inject Angular's $scope
  
 	app.controller('PaintingsCtrl', function($scope,  $http) {
+		$scope.filters = { };
 		$http.get("json/painting.json")
 		.success(function(response) {$scope.paintings = response.painting;});
+
+		
 
 		$scope.toggleInfo = function(selectedPainting){
 			//console.log('food object: ', selectedFood);
