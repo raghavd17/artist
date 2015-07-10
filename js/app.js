fdb1dd239b09096cf6521154759c06425fa63d57
Avnesh S <avnesh@neevtech.com>
2015-07-10 11:05:06 +0530

    html5mode for remove index.html from url

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ var app = angular.module('myApp', []);
 			//Uri /about -> template about.html and Controller ShowAboutController
 			app.config(['$routeProvider',
 			  function($routeProvider) {
+			  	$route.html5Mode(true);
 			    $routeProvider.
 			      when('/', {
 			        templateUrl: 'portfolio.html',
